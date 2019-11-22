ef3541d9d26303a99997066dbf7c9628663a466e
Sondre Rodahl <son.rodahl@gmail.com>
2019-11-22 10:44:58 +0100

    Added more tests for cosinesim

--- test/addressing_tests.jl
+++ test/addressing_tests.jl
@@ -1,4 +1,7 @@
 
 @testset "Cosine similarity" begin
     @test DNC.cosinesim([1, 2], [1, 2]) ≈ 1
+    @test DNC.cosinesim([1, 1], [0,1]) ≈ cos(π/4)
+    @test DNC.cosinesim([-1, 1], [1, 0]) ≈ cos(3π/4)
+    @test DNC.cosinesim([1, 0, 0], [0, 1, 0]) == 0
 end
