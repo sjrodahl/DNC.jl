c97a97f6624fa51d51604367ef491535effd9d06
Sondre Rodahl <son.rodahl@gmail.com>
2019-11-26 17:38:32 +0100

    Memory allocation implemented

--- src/addressing.jl
+++ src/addressing.jl
@@ -8,3 +8,29 @@ function contentaddress(key, M, β, K=cosinesim)
     xs = [K(key, row) for row in eachrow(M)]
     weighted_softmax(xs, β)
 end
+
+# Single read head
+function memoryretention(read_weights::AbstractArray{<:Number, 1}, free_gate)
+    return ones(length(read_weights)) .- free_gate.*read_weights
+end
+
+#  Multiple read heads
+function memoryretention(read_weights::Array{<:AbstractArray, 1}, free_gate)
+    R = length(read_weights)
+    rs = [ones(length(read_weights[i])) .- free_gate[i].*read_weights[i] for i in 1:R]
+    foldl(rs) do x, y
+        x.*y
+    end
+end
+
+usage(u_prev, write_weights, 𝜓) = (u_prev + write_weights - (u_prev.*write_weights)) .* 𝜓
+
+function allocationweighting(u)
+    N = length(u)
+    a = zeros(N)
+    ϕ = sortperm(u) # Indices in ascending order of usage
+    for j in 1:N
+        a[ϕ[j]] = (1-u[ϕ[j]])*foldl(*, [u[ϕ[i]] for i in 1:(j-1)])
+    end
+    a
+end
--- test/addressing_tests.jl
+++ test/addressing_tests.jl
@@ -18,3 +18,36 @@ import Flux: softmax
     w_c = contentaddress(key, M, β)
     @test w_c[1] == w_c[2]
 end
+
+
+@testset "Memory allocation" begin
+    usage_case_1 = (w_r = [0.5, 0.25, 0.25], f=1, w_w = [0.25, 0.5, 0.25])
+    usage_case_2 = (w_r = [[0.6, 0.3, 0.1], [0.0, 0.5, 0.5]], f=[1, 0], w_w = [0.25, 0.5, 0.25])
+    u_prev = [1.0, 0.0, 0.0]
+
+    @testset "Memory retention 𝜓" begin
+        @test DNC.memoryretention(usage_case_1.w_r, usage_case_1.f) == [0.5, 0.75, 0.75]
+        # Two read heads
+        @test DNC.memoryretention(usage_case_2.w_r, usage_case_2.f) == [0.4, 0.7, 0.9]
+    end
+
+    @testset "Usage u⃗" begin
+        w_r, f, w_w = usage_case_1
+        𝜓 = DNC.memoryretention(w_r, f)
+        @test DNC.usage(u_prev, w_w, 𝜓) == [1//2, 3//8, 3//16]
+        # Two read heads
+        w_r, f, w_w = usage_case_2
+        𝜓 = DNC.memoryretention(w_r, f)
+        @test DNC.usage(u_prev, w_w, 𝜓) == [0.4, 0.35, 0.225]
+    end
+
+    @testset "Allocation a⃗" begin
+        w_r, f, w_w = usage_case_2
+        u = DNC.usage(u_prev,w_w, DNC.memoryretention(w_r, f))
+        @test DNC.allocationweighting(u) ≈ [0.04725, 0.14625, 0.775]
+        # Allocation is zero if all usages are 1
+        u_1 = ones(5)
+        @test DNC.allocationweighting(u_1) == (zeros(5))
+    end
+
+end
