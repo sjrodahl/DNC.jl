14f921add17015e7897fdb36fabcd2b19144989c
Sondre Rodahl <son.rodahl@gmail.com>
2019-12-13 22:57:24 +0100

    Moved type definitions to access.jl

--- src/DNC.jl
+++ src/DNC.jl
@@ -14,41 +14,9 @@ export WriteHead
 export Dnc
 export DNCCell
 
-@with_kw mutable struct State{A<:AbstractArray, V<:AbstractArray}
-    L::Matrix
-    p::V
-    u::V
-    w_w::V
-    w_r::A
-end
 
-State(N::Int, R::Int) = State(
-    L=zeros(N, N),
-    p = zeros(N),
-    u = zeros(N),
-    w_w = zeros(N),
-    w_r = [zeros(N) for i in 1:R]
-    )
-
-@with_kw struct WriteHead{A<:AbstractArray, T<:Real}
-    k::A # Write key
-    β::T # Key strength
-    e::A # erase
-    v::A # add
-    g_a::T # allocation gate
-    g_w::T # write gate
-end
-
-@with_kw struct ReadHead{A<:AbstractArray, T<:Real}
-    k::A # read key
-    β::T # key strength
-    f::T # free gate
-    π::A # stractArray{A, 1} # read mode
-end
-
-
-include("computer.jl")
 include("access.jl")
+include("computer.jl")
 include("addressing.jl")
 include("utils.jl")
 
--- src/access.jl
+++ src/access.jl
@@ -1,3 +1,35 @@
+@with_kw mutable struct State{A<:AbstractArray, V<:AbstractArray}
+    L::Matrix
+    p::V
+    u::V
+    w_w::V
+    w_r::A
+end
+
+State(N::Int, R::Int) = State(
+    L=zeros(N, N),
+    p = zeros(N),
+    u = zeros(N),
+    w_w = zeros(N),
+    w_r = [zeros(N) for i in 1:R]
+    )
+
+@with_kw struct WriteHead{A<:AbstractArray, T<:Real}
+    k::A # Write key
+    β::T # Key strength
+    e::A # erase
+    v::A # add
+    g_a::T # allocation gate
+    g_w::T # write gate
+end
+
+@with_kw struct ReadHead{A<:AbstractArray, T<:Real}
+    k::A # read key
+    β::T # key strength
+    f::T # free gate
+    π::A # stractArray{A, 1} # read mode
+end
+
 # L should be updated before this
 function readmem(M, rh::ReadHead, L::Matrix, prev_w_r)
     @unpack k, β, π = rh
