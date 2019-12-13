2aed18198032b1bf8953de4782abae91b41a3352
Sondre Rodahl <son.rodahl@gmail.com>
2019-12-13 17:07:19 +0100

    Split update to after read and after write

--- src/access.jl
+++ src/access.jl
@@ -26,8 +26,7 @@ function writemem(M,
     newmem
 end
 
-function updatestate!(state, M, wh::WriteHead, rh::Vector{ReadHead})
-    free = [rh.f for rh in rhs]
+function update_state_after_write!(state::State, M, wh::WriteHead, free::AbstractArray)
     c_w = contentaddress(wh.k, M, wh.β)
     𝜓 = memoryretention(state.w_r, free)
     u = usage(state.u, state.w_w, 𝜓)
@@ -37,20 +36,13 @@ function updatestate!(state, M, wh::WriteHead, rh::Vector{ReadHead})
     state.w_w = w_w
     updatelinkmatrix!(state.L, state.p, state.w_w)
     state.p = precedenceweight(state.p, state.w_w)
+end
+
+function update_state_after_read!(state::State, M, rhs::AbstractArray)
     b = backwardweight(state.L, state.w_r)
     f = forwardweight(state.L, state.w_r)
     w_r = [readweight(b, c_r, f, rh.π) for rh in rhs]
     state.w_r = w_r
-end
-
-function updatestate!(state, M, interface)
-    c_w = contentaddress(interface.k_w, M, interface.β_w)
-    𝜓 = memoryretention(state.w_r, interface.free)
-    u = usage(state.u, state.w_w, 𝜓)
-    a = allocationweighting(u)
-    w_w = writeweight(c_w, a, interface.write_gate, interface.alloc_gate)
-    state.u = u
-    state.w_w = w_w
 end
 
 erase_and_add(M, w_w, e, a) = M .* (ones(size(M)) - w_w * e') + w_w * a'
