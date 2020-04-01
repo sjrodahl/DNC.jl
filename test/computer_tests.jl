9b76037ad375e92e6a5f654c33ef4684c0e259df
Sondre Rodahl <son.rodahl@gmail.com>
2020-04-01 10:26:52 +0200

    Type stability Float32 tests

--- test/computer_tests.jl
+++ test/computer_tests.jl
@@ -1,5 +1,8 @@
 using Flux
 
+using Random
+rng = MersenneTwister(2345)
+
 @testset "Computer runs without error" begin
     R = 1
     N = 5
@@ -8,16 +11,22 @@ using Flux
     Y = 2
     inputsize = DNC.inputsize(X, R, W)
     outputsize = DNC.outputsize(R, N, W, X, Y)
-    x = [1, 2]
-    y = [1, 2]
+    x = [1.f0, 2]
+    y = [1.f0, 2]
     controller = Chain(Dense(inputsize, R*W*2, relu),
                        Dense(R*W*2, R*W*2, relu),
                        Dense(R*W*2, outputsize, relu))
+    @test eltype(controller(rand(rng, Float32, inputsize))) == Float32
     controller2 = LSTM(inputsize, outputsize)
+    @test eltype(controller2(rand(rng, Float32, inputsize))) == Float32
     dnc1 = Dnc(controller, X, Y, N, W, R)
-    @test length(dnc1(x)) == Y
+    out1 = dnc1(x)
+    @test length(out1) == Y
+    @test eltype(out1) == Float32
     dnc2 = Dnc(controller2, X, Y, N, W, R)
-    @test length(dnc2(x)) == Y
+    out2 = dnc2(x)
+    @test length(out2) == Y
+    @test eltype(out2) == Float32
     @testset "Get gradient" begin
         for dnc in (dnc1, dnc2)
             g = gradient(x->sum(dnc(x)), x)
@@ -39,9 +48,9 @@ end
     dnc = Dnc(controller, X, Y, N, W, R)
 
     function generatedata(in, out, n=100)
-        w = rand(out, in)
-        b = rand(out)
-        Xs = [rand(in) for i in 1:n]
+        w = rand(rng, Float32, out, in)
+        b = rand(rng, Float32, out)
+        Xs = [rand(rng, Float32, in) for i in 1:n]
         Ys = [w*x .+ b for x in Xs]
         zip(Xs, Ys)
     end
@@ -51,5 +60,6 @@ end
     opt = ADAM(0.01)
     evalcb() = @show loss(first(data)[1], first(data)[2])
     Flux.train!(loss, params(dnc), data, opt, cb=Flux.throttle(evalcb, 10))
+    @test eltype(dnc.cell.memoryaccess.M) == Float32
     @test loss(first(data)[1], first(data)[2]) < 1.0
 end
