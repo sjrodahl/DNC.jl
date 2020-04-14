bbf9956c64f74df630bf91250dfa07e71c004859
Sondre Rodahl <son.rodahl@gmail.com>
2020-04-14 12:32:44 +0200

    Bathify addressing tests

--- test/addressing_tests.jl
+++ test/addressing_tests.jl
@@ -2,17 +2,21 @@ using Flux: σ
 using Zygote
 using DNC: contentaddress
 
-function generate_Sn(n)
-    vec = rand(Float32, n)
-    vec = exp.(vec)/sum(exp.(vec))
-    vec
+
+function generate_Sn(dims...)
+    arr = rand(Float32, dims...)
+    softmax(arr; dims=1)
 end
 
-function generate_Δn(n)
-    vec = rand(Float32, n)
-    vec = exp.(vec)/(sum(exp.(vec).+(rand(Float32, n)/2)))
-    vec
+
+function generate_Δn(dims...)
+    fuzzysoftmax(arr; dims=1) = 
+        exp.(arr)./(sum(exp.(arr); dims=dims) .+ rand(Float32, size(arr))/2)
+    arr = rand(Float32, dims...)
+    fuzzysoftmax(arr; dims=1)
 end
+
+
 
 in_Sn(vec) = sum(vec) == 1.0
 in_Δn(vec) = sum(vec) >= 0.0 && sum(vec) <= 1.0
@@ -47,7 +51,7 @@ state = State(
     @test eltype(u) == Float32
     a = DNC.allocationweighting(u)
     @test eltype(a) == Float32
-    gw, ga = rand(Float32, B), rand(Float32, B)
+    gw, ga = rand(Float32, 1, B), rand(Float32, 1, B)
     ww = DNC.writeweight(cw, a, gw, ga)
     @test eltype(ww) == Float32
     @test size(ww) == (N, 1, B)
@@ -67,79 +71,86 @@ state = State(
 end
 
 @testset "Content-based addressing" begin
-    M =Float32[0.1 0.5 1.5;
-                  -1.2 0.8 0.0]
-    key = Matrix(M')
-    key[1, 1] = 0 # Avoid exact match
-    β = [100.0f0, 100.0f0]
+    M =reshape(Float32[0.1 0.5 1.5;
+                       -1.2 0.8 0.0], 2, 3, 1)
+    key = rand(Float32, 3, 1, 1)
+    key[:, 1, 1] = M[1, : , 1]
+    key[1, 1, 1] = 0 # Avoid exact match
+    β = reshape([100.0f0, 100.0f0], 1, 2)
     # With a high sharpener β, the match is found
     res = contentaddress(key, M, β)
     @test res[1] ≈ 1
     @test eltype(res) == Float32
-    # Should return equal values for parallel memory rows
-    M = Float32[1 0; 2 0]
-    key = Float32.(Matrix([1 1]'))
-    β = [1.f0]
-    wc = contentaddress(key, M, β)
-    @test wc[1] == wc[2]
 end
 
 
 @testset "Memory allocation" begin
-    usagecase1 = (wr = Float32.(Matrix([0.5 0.25 0.25]')), f=[1.f0], ww = [0.25f0, 0.5f0, 0.25f0])
-    usagecase2 = (wr =Float32.([0.6 0.0;
-                        0.3 0.5;
-                        0.1 0.5]),
-                  f=[1.f0, 0.f0],
-                  ww = [0.25f0, 0.5f0, 0.25f0])
-    u_prev = [1.0f0, 0.0f0, 0.0f0]
-    state2 = State(3, 2)
-    state2.ww = usagecase2.ww
-    state2.wr = usagecase2.wr
+    N, W, R, B = 3, 4, 1, 1
+    wr1 = rand(Float32, N, R, B)
+    wr1[:, 1, 1] = [0.5f0, 0.25f0, 0.25f0]
+    f1 = ones(Float32, R, B)
+    ww1 = rand(Float32, N, 1, B)
+    ww1[:, 1, 1] = [0.25f0, 0.5f0, 0.25f0]
+    case1 = (wr=wr1, f=f1, ww=ww1)
+    R = 2
+    wr2 = rand(Float32, N, R, B)
+    wr2[:, :, 1] = Float32.([0.6 0.0;
+                             0.3 0.5;
+                             0.1 0.5])
+    f2 = rand(Float32, R, B)
+    f2[:, 1] = [1.f0, 0.f0]
+    ww2 = rand(Float32, N, 1, B)
+    ww2[:, 1, 1] = [0.25f0, 0.5f0, 0.25f0]
+    case2 = (wr=wr2, f=f2, ww=ww2)
+    u_prev = rand(Float32, N, B)
+    u_prev[:, 1] = [1.0f0, 0.0f0, 0.0f0]
+    state2 = State(N, R, B)
+    state2.ww = case2.ww
+    state2.wr = case2.wr
     state2.u = u_prev
 
     @testset "Memory retention 𝜓" begin
-        memret = DNC.memoryretention(usagecase1.wr, usagecase1.f) 
-        @test memret == [0.5, 0.75, 0.75]
+        memret = DNC.memoryretention(case1.wr, case1.f) 
+        @test memret[:, 1] == [0.5, 0.75, 0.75]
         @test eltype(memret) == Float32
         # Two read heads
-        memret = DNC.memoryretention(usagecase2.wr, usagecase2.f)
-        @test isapprox(memret, [0.4, 0.7, 0.9], atol=1e-5)
+        memret = DNC.memoryretention(case2.wr, case2.f)
+        @test isapprox(memret[:, 1], [0.4, 0.7, 0.9], atol=1e-5)
         @test eltype(memret) == Float32
     end
 
     @testset "Usage u⃗" begin
-        wr, f, ww = usagecase1
+        wr, f, ww = case1
         u = DNC.usage(u_prev, ww, wr, f)
-        @test u == [1//2, 3//8, 3//16]
+        @test u[:, 1] == [1//2, 3//8, 3//16]
         @test eltype(u) == Float32
         # Two read heads
-        wr, f, ww = usagecase2
+        wr, f, ww = case2
         u = DNC.usage(u_prev, ww, wr, f)
-        @test isapprox(u, [0.4, 0.35, 0.225], atol=1e-5)
+        @test isapprox(u[:, 1], [0.4, 0.35, 0.225], atol=1e-5)
         @test eltype(u) == Float32
     end
 
     @testset "Allocation a⃗" begin
         # Using approximation due to DNC's use of _EPSILON to avoid num. instability
-        wr, f, ww = usagecase2
+        wr, f, ww = case2
         u = DNC.usage(u_prev,ww, wr, f)
         alloc = DNC.allocationweighting(u)
         @test eltype(alloc) == Float32
-        @test isapprox(alloc, [0.04725, 0.14625, 0.775]; atol=DNC._EPSILON*10)
+        @test isapprox(alloc[:, 1], [0.04725, 0.14625, 0.775]; atol=DNC._EPSILON*10)
         # Allocation is zero if all usages are 1
-        allused = ones(5)
-        @test isapprox(DNC.allocationweighting(allused), (zeros(5)); atol=DNC._EPSILON*10)
+        allused = ones(5, B)
+        @test isapprox(DNC.allocationweighting(allused), (zeros(5, B)); atol=DNC._EPSILON*10)
         # First location is allocated if all is available
-        noneused = zeros(5)
-        @test isapprox(DNC.allocationweighting(noneused), [1.0, 0, 0, 0, 0]; atol=DNC._EPSILON*10)
+        noneused = zeros(5, B)
+        @test isapprox(DNC.allocationweighting(noneused)[:, 1], [1.0, 0, 0, 0, 0]; atol=DNC._EPSILON*10)
     end
 
     @testset "Write weighting ww" begin
-        ga = 0.f0
-        gw = 1.f0
-        a = generate_Δn(3)
-        cw = generate_Sn(3)
+        ga = zeros(Float32, 1, B)
+        gw = ones(Float32, 1, B)
+        a = generate_Δn(N, B)
+        cw = generate_Sn(N, 1, B)
         ww = DNC.writeweight(cw, a, gw, ga) 
         @test ww == cw
         @test eltype(ww) == Float32
@@ -148,56 +159,62 @@ end
 
 @testset "Temporal linkage" begin
     @testset "Precedence weights p⃗" begin
-        ww = [0.1f0, 0.2f0, 0.3f0, 0.4f0]
-        p0 = zeros(Float32, 4)
+        N, B = 4, 1
+        ww = rand(Float32, N, 1, B)
+        ww[:, :, 1] = [0.1f0, 0.2f0, 0.3f0, 0.4f0]
+        p0 = zeros(Float32, N, B)
         p = DNC.precedenceweight(p0, ww) 
-        @test p == ww
+        @test p == dropdims(ww; dims=2)
         @test eltype(p) == Float32
     end
 
     # Write weights. Writes to location 1, 2, and 3 iteratively.
-    ww = [[1.0f0, 0.0f0, 0.0f0],
+    N, R, B = 3, 1, 1
+    ww = rand(Float32, N, 1, B)
+    wwlist = [[1.0f0, 0.0f0, 0.0f0],
             [0.0f0, 1.0f0, 0.0f0],
             [0.0f0, 0.0f0, 1.0f0]]
     # Temporal link matrix
-    L = Matrix(zeros(Float32, 3, 3))
+    L = zeros(Float32, N, N, B)
     # precedence weights
-    p = zeros(Float32, 3)
+    p = zeros(Float32, N, B)
     # Expected evolution of L
-    expected = [
-        zeros(Float32, 3, 3),
-        [0.0f0 0.0f0 0.0f0;
-        1.0f0 0.0f0 0.0f0;
-        0.0f0 0.0f0 0.0f0],
-        [0.0f0 0.0f0 0.0f0;
-        1.0f0 0.0f0 0.0f0;
-        0.0f0 1.0f0 0.0f0]
-    ]
+    expected = [zeros(Float32, N, N, B) for i in 1:3]
+    expected[2][2, 1, 1] = 1.0f0
+    expected[3][2, 1, 1] = 1.0f0
+    expected[3][3, 2, 1] = 1.0f0
+
     @testset "Link matrix L" begin
-        for i in 1:length(ww)
-            DNC.updatelinkmatrix!(L, p, ww[i])
+        for i in 1:length(wwlist)
+            ww[:, :, 1] = wwlist[i]
+            DNC.updatelinkmatrix!(L, p, ww)
             @test L == expected[i]
             @test eltype(L) == Float32
-            p = DNC.precedenceweight(p, ww[i])
+            p = DNC.precedenceweight(p, ww)
         end
         # Last read was location 2. The forward weight should point to 3, backward to 1.
-        wr = [0.f0, 1, 0]
+        wr = rand(Float32, N, R, B)
+        wr[:, :, 1] = [0.f0, 1, 0]
         forward = DNC.forwardweight(L, wr)
         backward = DNC.backwardweight(L, wr)
-        @test  forward == [0, 0, 1]
-        @test backward == [1, 0, 0]
+        @test  forward[:, 1] == [0, 0, 1]
+        @test backward[:, 1] == [1, 0, 0]
         @test eltype(forward) == Float32
         @test eltype(backward) == Float32
     end
 
     @testset "Read weighting w_t^r" begin
-        b = [0.f0, 0, 1]
-        f = [1.f0, 0, 0]
-        c = [0.f0, 1, 0]
-        pi = [0.f0, 1, 0]
-        readw = DNC._readweight(b, c, f, pi)
+        b = reshape([0.f0, 0, 1], 3, 1, 1)
+        f = reshape([1.f0, 0, 0], 3, 1, 1)
+        c = reshape([0.f0, 1, 0], 3, 1, 1)
+        readmode = reshape([0.f0, 1, 0], 3, 1, 1)
+        readw = DNC.readweight(b, c, f, readmode)
+        @test readw == c
         @test in_Δn(readw)
         @test eltype(readw) == Float32
+        readmode2 = reshape([0.5f0, 0.5f0, 0.0f0], 3, 1, 1)
+        readw = DNC.readweight(b, c, f, readmode2)
+        @test readw == 0.5f0b .+ 0.5f0c
     end
 
 end
